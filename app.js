7ad484aff70fce0fd7a1fb2a9d58a1080333eaf5
MathieuMolina <molina.mathieu@hotmail.com>
2022-02-10 09:33:43 +0100

    feat: logical function : ask/check/convert

--- app.js
+++ app.js
@@ -14,3 +14,18 @@ function multiplication(nb1,nb2){
     let calcul = nb1*nb2;
     alert(calcul);
 }
+function demandeNombre(){
+    nb1= prompt("Sélectionner un premier chiffre pour l'opération");
+    nb2= prompt("Sélectionner un second chiffre à opérer avec le premier chiffre");
+}
+function controlNombre(nb1,nb2){
+    console.log("controlNombre");
+    if(isNaN(nb1) || isNaN(nb2)){
+        alert("Veuillez saisir des nombres");
+        general();
+    }
+}
+function conversionNombre(nb1,nb2){
+    nbConvert1 = parseInt(nb1);
+    nbConvert2 = parseInt(nb2);
+}
